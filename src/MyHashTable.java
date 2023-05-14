a01ea03ece16b31b1ec6b8ca1aef3dbe3190ed7f
Alikhan <astanaproteam123@gmail.com>
2023-05-14 15:01:43 +0600

    getKey method

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -92,4 +92,17 @@ public class MyHashTable<K, V> {
         }
         return false;
     }
+    public K getKey(V value){
+        for (int i = 0; i < M; i++) {
+            HashNode<K,V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
 }

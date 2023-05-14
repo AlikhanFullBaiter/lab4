2909f33d4f18a2ae6a36594ea77a3cb2d6c689c1
Alikhan <astanaproteam123@gmail.com>
2023-05-14 15:30:22 +0600

    documentation to Student class

--- src/Student.java
+++ src/Student.java
@@ -1,22 +1,46 @@
 package src;
 
+/**
+ * Represents a student with a name and age.
+ */
 public class Student {
     private int age;
     private String name;
 
+    /**
+     * Constructs a Student object with the specified name and age.
+     *
+     * @param name the name of the student
+     * @param age  the age of the student
+     */
     public Student(String name , int age) {
         this.name  = name;
         this.age = age;
     }
 
+    /**
+     * Retrieves the name of the student.
+     *
+     * @return the name of the student
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Retrieves the age of the student.
+     *
+     * @return the age of the student
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Generates a hash code for the Student object based on its name and age.
+     *
+     * @return the hash code value
+     */
     @Override
     public int hashCode() {
         int prime = 31;
@@ -25,6 +49,12 @@ public class Student {
         result = prime * result + age;
         return result;
     }
+    /**
+     * Checks if the Student object is equal to another object.
+     *
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -39,6 +69,11 @@ public class Student {
 
 
 
+    /**
+     * Returns a string representation of the Student object.
+     *
+     * @return a string representation of the object
+     */
     @Override
     public String toString() {
         return "Student{" +

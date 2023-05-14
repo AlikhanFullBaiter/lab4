ae93c780ceebafcbc25085eade67475bc244450d
Alikhan <astanaproteam123@gmail.com>
2023-05-14 15:15:42 +0600

    constructor

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,10 @@ package src;
 public class Student {
     private int age;
     private String name;
+
+    public Student(String name , int age) {
+        this.name  = name;
+        this.age = age;
+    }
+
 }

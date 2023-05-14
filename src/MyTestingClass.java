8490a8947e4efe6c4ff3c33c35579dc6d469984f
Alikhan <astanaproteam123@gmail.com>
2023-05-14 15:27:52 +0600

    documentation to MyTestingClass

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,14 +1,28 @@
 package src;
 
+/**
+ * Represents a sample testing class with an ID and a name.
+ */
 public class MyTestingClass {
     private int id;
     private String name;
 
+    /**
+     * Constructs a MyTestingClass object with the specified ID and name.
+     *
+     * @param id   the ID of the testing class
+     * @param name the name of the testing class
+     */
     public MyTestingClass(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Generates a hash code for the MyTestingClass object based on its ID and name.
+     *
+     * @return the hash code value
+     */
     @Override
     public int hashCode() {
         int prime = 31;
@@ -20,6 +34,12 @@ public class MyTestingClass {
         return result;
     }
 
+    /**
+     * Checks if the MyTestingClass object is equal to another object.
+     *
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -31,6 +51,13 @@ public class MyTestingClass {
         MyTestingClass other = (MyTestingClass) obj;
         return this.id == other.id && this.name.equals(other.name);
     }
+
+
+    /**
+     * Returns a string representation of the MyTestingClass object.
+     *
+     * @return a string representation of the object
+     */
 
     @Override
     public String toString() {

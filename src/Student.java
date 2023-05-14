0e16e42f86e6253e8ba05cc9b0c3d8c55d86f124
Alikhan <astanaproteam123@gmail.com>
2023-05-14 15:19:32 +0600

    equals , hashcode and toString methods

--- src/Student.java
+++ src/Student.java
@@ -16,4 +16,34 @@ public class Student {
     public int getAge() {
         return age;
     }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 5;
+        result = prime * result + name.hashCode();
+        result = prime * result + age;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Student)) {
+            return false;
+        }
+        Student other = (Student) obj;
+        return this.name.equals(other.name) && this.age == other.age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
